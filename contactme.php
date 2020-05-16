bb26a8a57b81db536cbf5e2a57e3c1e506655e16
GitHub-ccd <chathurnagad@gmail.com>
2020-05-16 18:21:44 -0500

    checnge contact me

---
+++ contactme.php
@@ -0,0 +1,23 @@
+<?php
+
+$to = "chathurangad@gmail.com";
+
+$email_subject = "Contact form submission: $name";
+
+$email_body = "You have received a new message. ".
+
+" Here are the details:\n Name: $name \n ".
+
+"Email: $email_address\n Message \n $message";
+
+$headers = "From: $myemail\n";
+
+$headers .= "Reply-To: $email_address";
+
+mail($to,$email_subject,$email_body,$headers);
+
+//redirect to the 'thank you' page
+
+
+
+?>
